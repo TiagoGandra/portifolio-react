ea497611d7a8a1cc8653dfd9ab02ea416ce18f86
tiago.gandra <tiagogandrati@gmail.com>
2025-03-25 10:40:30 -0300

    feat: project images and card structure

--- src/images/index.js
+++ src/images/index.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg'
-import tour1 from './tour-1.jpeg'
-import tour2 from './tour-2.jpeg'
-import tour3 from './tour-3.jpeg'
-import tour4 from './tour-4.jpeg'
-import tour5 from './tour-5.jpeg'
-import tour6 from './tour-6.jpeg'
-import about from './about.jpeg'
+import logo from "./logo.svg";
+import tour1 from "./tour-1.jpeg";
+import tour2 from "./tour-2.jpeg";
+import tour3 from "./tour-3.jpeg";
+import tour4 from "./tour-4.jpeg";
+import tour5 from "./tour-5.jpeg";
+import tour6 from "./tour-6.jpeg";
+import about from "./about.jpeg";
+import demeter from "./demeter.png";
+import svpeer from "./peer-to-peer.png";
+import gymspike from "./GymSpike.jpg";
+import nlwia from "./nlwia.png";
+import nlwpython from "./nlwpython.png";
+import backroads from "./backroads.png";
 
-
-export {logo, tour1, tour2, tour3, tour4, tour5, tour6, about};
+export {
+  logo,
+  tour1,
+  tour2,
+  tour3,
+  tour4,
+  tour5,
+  tour6,
+  about,
+  demeter,
+  svpeer,
+  gymspike,
+  nlwia,
+  nlwpython,
+  backroads,
+};
